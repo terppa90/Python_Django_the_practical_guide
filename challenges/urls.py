10ea7cabc3911246fc00464e8622116dc874d2ae
Tero <teromese@hotmail.com>
2022-02-23 11:54:51 +0200

    URLs and views

---
+++ challenges/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+# from the same folder (.)
+from . import views
+# If the request reaches /january, execute the index function from views.
+# <month> is a dynamic path segment
+urlpatterns = [
+    #path("january", views.january),
+    path("<str:month>", views.monthly_challenge, name="month-challenge"),
+    # path converter (int)
+    path("<int:month>", views.monthly_challenge_by_number),
+    path("", views.index)
+]
